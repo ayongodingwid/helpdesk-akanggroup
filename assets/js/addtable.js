d7ccaf1beac6dc9a802172f85d42a479fb5e7142
ayongodingwid <ayongodingwid@gmail.com>
2025-01-27 12:41:16 +0700

    Perbaikan halaman dashboard
    Penambahan beberapa halaman untuk melengkapi kekurangan sebelumnya.

--- assets/js/addtable.js
+++ assets/js/addtable.js
@@ -36,6 +36,94 @@ $(document).ready(function() {
         $("#asset-table tbody").append(newRow);
       });
 
+      $("#add-table-discount").on("click", function() {
+        const newRow = `
+        <tr>
+                        <td>
+                          <select class="form-select" id="Sales Mode">
+                            <option selected></option>
+                            <option value="1">Grab Food</option>
+                            <option value="2">Go Food</option>
+                            <option value="3">Shopee Food</option>
+                            <option value="4">Dine In</option>
+                            <option value="4">Take Away</option>
+                        </select>
+                        </td>                     
+                        <td>
+                          <select class="form-select" id="Sales Mode">
+                            <option selected></option>
+                            <option value="1">Rp.</option>
+                            <option value="2">%</option>
+                        </select>
+                        </td> 
+                        <td><input type="text" class="form-control"></td>
+                        <td><input class="form-check-input" type="checkbox" id="gridCheck2"></td>   
+                        <td>
+                          <button class="btn btn-danger btn-sm remove-row"><i class="bi bi-x-circle"></i></button>
+                        </td>
+                      </tr>`;
+        $("#discount-table tbody").append(newRow);
+      });
+
+      $("#add-table-price").on("click", function() {
+        const newRow = `
+        <tr>
+          <td>
+            <select class="form-select" id="Sales Mode">
+              <option selected></option>
+              <option value="1">Grab Food</option>
+              <option value="2">Go Food</option>
+              <option value="3">Shopee Food</option>
+              <option value="4">Dine In</option>
+              <option value="4">Take Away</option>
+            </select>
+          </td>                     
+          <td><input type="text" class="form-control"></td>
+          <td><input type="text" class="form-control"></td>
+          <td><input type="text" class="form-control"></td>
+          <td><input class="form-check-input" type="checkbox" id="gridCheck2"></td>   
+          <td>
+            <button class="btn btn-danger btn-sm remove-row"><i class="bi bi-x-circle"></i></button>
+          </td>
+        </tr>
+        `;
+        $("#price-table tbody").append(newRow);
+      });
+
+      $("#add-table-coa").on("click", function() {
+        const newRow = `
+        <tr>
+                        <td>
+                          <select class="form-select" id="Sales Mode">
+                            <option selected></option>
+                            <option value="1">Penambahan</option>
+                            <option value="2">Perubahan</option>
+                            <option value="3">Penghapusan</option>
+                        </select>
+                        </td>                     
+                        <td><input type="text" class="form-control"></td>
+                        <td><input type="text" class="form-control"></td>
+                        <td>
+                          <button class="btn btn-danger btn-sm remove-row"><i class="bi bi-x-circle"></i></button>
+                        </td>
+                      </tr>
+        `;
+        $("#coa-table tbody").append(newRow);
+      });
+
+      $("#add-table-subticket").on("click", function() {
+        const newRow = `
+        <tr>                
+          <td><input type="text" class="form-control"></td>
+          <td><input type="text" class="form-control"></td>
+          <td>
+            <button class="btn btn-danger btn-sm remove-row"><i class="bi bi-x-circle"></i></button>
+          </td>
+        </tr>
+        `;
+        $("#subticket-table tbody").append(newRow);
+      });
+
       // Remove a row from the table
       $(document).on("click", ".remove-row", function() {
         $(this).closest("tr").remove();
